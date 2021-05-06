c92d72dce82c6806e574199095e87edef7d63cac
SimonasVsm <simonas.kersulis@visma.com>
2021-05-06 08:47:20 +0300

    Working with PR comments

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const previousImage = document.getElementById('left-btn')
-const nextImage = document.getElementById('right-btn')
-const carouselImages = document.querySelectorAll('.carousel__image')
-const carouselIndicatorButtons = document.querySelectorAll(
-	'.carousel__indicator'
-)
+const ui = {
+	buttonNext: document.getElementById('right-btn'),
+	buttonPrevious: document.getElementById('left-btn'),
+	carouselImages: document.querySelectorAll('.carousel__image'),
+	carouselIndicatorButtons: document.querySelectorAll('.carousel__indicator'),
+	// get currentImageElement() {
+	//   return document.querySelectorAll(
+	//     '.carousel__indicator'
+	//   )
+	// }
+}
 
 let currentImage = 1
-const totalImages = carouselImages.length
+// const totalImages = ui.carouselImages.length
 
 function removeActiveNavButtonClass() {
-	carouselIndicatorButtons.forEach((button) => {
+	ui.carouselIndicatorButtons.forEach((button) => {
 		button.classList.remove('carousel__indicator--active')
 	})
 }
 
 function removeActiveImageClass() {
-	carouselImages.forEach((image) => {
+	ui.carouselImages.forEach((image) => {
 		image.classList.remove('carousel__image--active')
 	})
 }
@@ -29,43 +34,44 @@ function addActiveNavButtonClass(index) {
 	document.getElementById(index).classList.add('carousel__indicator--active')
 }
 
-previousImage.addEventListener('click', () => {
+ui.buttonPrevious.addEventListener('click', () => {
 	removeActiveImageClass()
 
 	if (currentImage - 1 < 0) {
-		addActiveImageClass(carouselImages[carouselImages.length - 1])
+		addActiveImageClass(ui.carouselImages[ui.carouselImages.length - 1])
 
-		currentImage = carouselImages.length
+		currentImage = ui.carouselImages.length
 
 		addActiveNavButtonClass(currentImage)
 		currentImage--
 	} else {
-		addActiveImageClass(carouselImages[currentImage - 1])
+		addActiveImageClass(ui.carouselImages[currentImage - 1])
 
 		addActiveNavButtonClass(currentImage)
 		currentImage--
 	}
 })
 
-nextImage.addEventListener('click', () => {
+ui.buttonNext.addEventListener('click', () => {
 	removeActiveImageClass()
+	const isLast = currentImage + 1 >= ui.carouselImages.length + 1
 
-	if (currentImage + 1 >= carouselImages.length + 1) {
-		addActiveImageClass(carouselImages[carouselImages.length - 1])
+	if (isLast) {
+		addActiveImageClass(ui.carouselImages[ui.carouselImages.length - 1])
 
 		currentImage = 1
 		addActiveNavButtonClass(currentImage)
 	} else {
-		addActiveImageClass(carouselImages[currentImage - 1])
+		addActiveImageClass(ui.carouselImages[currentImage - 1])
 
 		currentImage++
 		addActiveNavButtonClass(currentImage)
 	}
 })
 
-carouselIndicatorButtons.forEach((button) => {
+ui.carouselIndicatorButtons.forEach((button) => {
 	button.addEventListener('click', () => {
-		const imageSelected = carouselImages[button.id - 1]
+		const imageSelected = ui.carouselImages[button.id - 1]
 
 		currentImage = button.id
 
